348de0073e2ae7726ecdea26372f981f239fcbe9
Mazhar Hassan <netsurgeon@gmail.com>
2023-02-27 08:14:09 +0500

    Initial commit with hello world
    access the url on localhost:8080 it will present a simple page where if message sent server will respond

---
+++ src/main/java/com/o4/open/mobility/PingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.o4.open.mobility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/o4/open/mobility/PingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.o4.open.mobility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
